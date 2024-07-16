df828d5edd7f0270e44b5a79414bde7ad4fa0f8f
jaydenzhao <378961928@qq.com>
2024-07-16 23:55:07 +0800

    update java

--- java/HelloSprintBoot/src/test/java/com/jayden/HelloSprintBoot/HelloSprintBootApplicationTests.java
+++ java/HelloSprintBoot/src/test/java/com/jayden/HelloSprintBoot/HelloSprintBootApplicationTests.java
@@ -1,13 +1,24 @@
 package com.jayden.HelloSprintBoot;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.redis.core.RedisTemplate;
 
 @SpringBootTest
 class HelloSprintBootApplicationTests {
+	@Autowired
+	private RedisTemplate redisTemplate;
 
 	@Test
-	void contextLoads() {
+	void testSet() {
+		redisTemplate.boundValueOps("name").set("luca");
+	}
+
+	@Test
+	void testGet() {
+		Object name = redisTemplate.boundValueOps("name").get();
+		System.out.println(name);
 	}
 
 }
